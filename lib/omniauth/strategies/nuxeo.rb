82acac8e24a2a18211af7110cb31807a52925838
briri <brian.riley@ucop.edu>
2018-10-05 15:25:19 -0700

    added info and extra and uid

--- lib/omniauth/strategies/nuxeo.rb
+++ lib/omniauth/strategies/nuxeo.rb
@@ -1,3 +1,4 @@
+require 'securerandom'
 require 'omniauth-oauth2'
 
 # OmniAuth strategy for connecting to the Nuxeo system via the OAuth 2.0 protocol
@@ -43,26 +44,50 @@ module OmniAuth
         @options.client_secret = args[0][:client_secret] || "1234567890"
         @options.client_options.site = args[0][:site] || "https://tester.nuxeo.org"
         @options.client_options.authorize_url = "#{args[0][:site]}/nuxeo/oauth2/authorize"
-        @options.client_options.token_url = "#{args[0][:site]}/token"
+        @options.client_options.token_url = "#{args[0][:site]}/nuxeo/oauth2/token"
+
+        @options.client_options.code_challenge_method = "S256"
+        @options.client_options.code_challenge = SecureRandom.base64(256)
       end
 
-      uid { raw_info["code"] }
+      uid { raw_info['id'] }
 
       info do
         {
-          name: raw_info["name"],
-          email: raw_info["email"]
+          'response' => raw_info,
+          'email' => raw_info['email']
         }
       end
 
       extra do
-        {
-          raw_info: raw_info
-        }
+        hash = {}
+        hash['raw_info'] = raw_info unless skip_info?
+        hash
       end
 
+      def authorize_params
+        super.tap do |params|
+          params[:client_secret] = @options.client_secret
+          #params[:code_challenge_method] = @options.client_options.code_challenge_method
+          #params[:code_challenge] = @options.client_options.code_challenge
+        end
+      end
+
+      def token_params
+        super.tap do |params|
+p "TOKEN CHECK #{ @env['omniauth.params'].inspect }"
+        end
+      end
+
+      def callback_phase
+        request.params["grant_type"] = "authorization_code"
+        request.params["client_id"] = @options.client_id
+      end
+
+      private
+
       def raw_info
-        @raw_info ||= access_token.get("/me").parsed
+        @raw_info ||= access_token.get('me', info_options).parsed || {}
       end
 
     end
